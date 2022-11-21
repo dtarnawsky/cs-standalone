cb7d6a0def01da3f592a8071c61f2ca8f9d18730
Damian Tarnawsky <damian@ionic.io>
2022-11-21 12:52:21 -0800

    Fix bootstrap app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routing';
@@ -12,9 +12,9 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(routes),
     importProvidersFrom(
       IonicModule.forRoot(),
-      RouterModule.forRoot(routes),
     )
   ]
 })
